f64498ba9345ba5c8d171f6914fe203f8160b7d4
Steve Lewis <stevelle@gmail.com>
2019-10-16 07:55:43 -0700

    Add a form to allow adding books
    Work to implement storage of books submitted will follow

---
+++ bookstore/main.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask, render_template
+
+from forms import AddBookForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = (os.environ.get('GAE_APPLICATION', 'super') +
+                            os.environ.get('GAE_DEPLOYMENT_ID', 'secret'))
+
+
+@app.route('/books/add', methods=['GET', 'POST'])
+def submit_add():
+    form = AddBookForm()
+    if form.validate_on_submit():
+        pass  # TODO
+
+    return render_template('add.html', form=form), 200
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
